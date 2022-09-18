bc5cb6401276db62bdb530537d41af77161655da
chaerlo127 <monynony0203@gmail.com>
2022-09-18 22:41:37 +0900

    create repo

---
+++ src/main/java/com/example/affiliates/AffiliatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.affiliates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffiliatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffiliatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/affiliates/AffiliatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.affiliates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffiliatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
