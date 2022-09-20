907363b69ef1edf8fdc8f3d243781743d63d5867
chaerlo127 <monynony0203@gmail.com>
2022-09-20 23:45:23 +0900

    [FEAT] 로그인 & 회원가입 기능

--- src/main/java/com/example/affiliates/User/Controller/UserController.java
+++ src/main/java/com/example/affiliates/User/Controller/UserController.java
@@ -1,11 +1,12 @@
 package com.example.affiliates.User.Controller;
 
 import com.example.affiliates.User.DTO.UserDTO;
+import com.example.affiliates.User.Entity.UserEntity;
 import com.example.affiliates.User.Service.UserService;
+import com.example.affiliates.Util.BaseException;
+import com.example.affiliates.Util.BaseResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/users")
@@ -15,9 +16,25 @@ public class UserController {
     public UserController(UserService userService){
         this.userService = userService;
     }
-
+    @ResponseBody
     @PostMapping("/login")
-    public void Login(@RequestBody UserDTO.Login user){
-        this.userService.login(user);
+    public  BaseResponse<UserEntity> Login(@RequestBody UserDTO.Login user){
+        try {
+            return new BaseResponse<>(this.userService.login(user));
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/sign-in")
+    public BaseResponse<String> signIn(@RequestBody UserDTO.Login user){
+        try {
+            this.userService.signIn(user);
+            return new BaseResponse<>("회원가입에 성공했습니다.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+
     }
 }
---
+++ src/main/java/com/example/affiliates/Util/LoginStatus.java
@@ -0,0 +1,5 @@
+package com.example.affiliates.Util;
+
+public enum LoginStatus {
+    LOGIN, LOG_OUT;
+}
