b4ae703a3fe74c9b7349ed799654cfb822da2bb1
otndus <xuyeon219@gmail.com>
2022-09-28 00:51:32 +0900

    [FEAT] 리뷰 작성 api 생성

--- src/main/java/com/example/affiliates/Store/Controller/StoreController.java
+++ src/main/java/com/example/affiliates/Store/Controller/StoreController.java
@@ -6,9 +6,13 @@ import com.example.affiliates.User.DTO.UserDTO;
 import com.example.affiliates.User.Service.UserService;
 import com.example.affiliates.Util.BaseException;
 import com.example.affiliates.Util.BaseResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -20,5 +24,24 @@ public class StoreController{
         this.storeService = storeService;
     }
 
+    /*
+     * 오수연 : 리뷰작성
+     * */
+    @ResponseBody
+    @ApiOperation(value = "리뷰작성 API")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "요청에 성공하였습니다."),
+            @ApiResponse(code = 2008, message = "storeIdx가 공백입니다."),
+            @ApiResponse(code = 2009, message = "별점을 입력해주세요.")
+    })
+    @PostMapping("/review")
+    public BaseResponse<String> storeReview(Principal principal, @RequestBody StoreDTO.GetStoreReview store){
+        try {
+            storeService.storeReview(principal, store);
+            return new BaseResponse<>("리뷰 작성을 완료했습니다.");
+        }catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 }
 
---
+++ src/main/java/com/example/affiliates/Store/Repository/ReviewRepository.java
@@ -0,0 +1,8 @@
+package com.example.affiliates.Store.Repository;
+
+import com.example.affiliates.Store.Entity.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+
+}
